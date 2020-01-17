1b3bde1fa1144353024448bedf096660d556d87a
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-17 10:46:38 -0800

    Add routing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,60 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+    if params[:sort]
+      case params[:sort]
+      when 'most_reviews'
+        @products = Product.most_reviews
+      when 'local'
+        @products = Product.origin
+      when 'recently_added'
+        @products = Product.recently_added
+      end
+    end
+    render :index
+    end
+
+    def new
+      @product = Product.new
+    end
+
+    def create
+      @product = Product.new(product_params)
+      if @product.save
+        flash[:notice] = "#{@product.name} has been saved to Products!"
+        redirect_to products_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @product = Product.find(params[:id])
+    end
+
+    def show
+      @product = Product.find(params[:id])
+      @reviews = @product.reviews
+    end
+
+    def update
+      @product = Product.find(params[:id])
+      if @product.update(product_params)
+        redirect_to product_path
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @product = Product.find(params[:id])
+      @product.destroy
+      redirect_to products_path
+    end
+
+    private
+    def product_params
+      params.require(:product).permit(:name, :cost, :origin)
+    end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,57 @@
 class ReviewsController < ApplicationController
 
+  def index
+    @reviews = Review.all
+    render :index
+    end
 
-  
+    def new
+      @products = Product.all
+      @review = Review.new
+      render :new
+    end
+
+    def create
+      @review = Review.new(review_params)
+      if @review.save
+        product = Product.find(params[:product_id])
+        flash[:notice] = "#{review.author} your review for #{product.name} has been saved!"
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @product = Product.find(params[:product_id])
+      @review = Review.find(params[:id])
+      redner :edit
+    end
+
+    def show
+      @product = Product.find(params[:product_id])
+      @review = Review.find(params[:id])
+      render :show
+    end
+
+    def update
+      @review = Review.find(params[:id])
+      if @review.update(review_params)
+        redirect_to reviews_path
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @product = Product.find(params[:product_id])
+      @review = Review.find(params[:id])
+      @review.destroy
+      redirect_to product_path
+    end
+
+    private
+    def review_params
+      params.require(:review).permit(:author, :rating, :content)
+    end
+
 end
