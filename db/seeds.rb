811a34530848fd675b6f8f08d540c9b40b8b2bd3
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-18 01:46:01 -0800

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+20.times do |index|
+  Product.create(name: Faker::Food.unique.spice, cost: Faker::Number.decimal(r_digits: 2), origin: Faker::Address.country)
+end
+20.times do |index|
+  Product.create(name: Faker::Food.unique.ingredient, cost: Faker::Number.decimal(r_digits: 2), origin: Faker::Address.country)
+end
+10.times do |index|
+  Product.create(name: Faker::House.unique.furniture, cost: Faker::Number.decimal(r_digits: 2), origin: Faker::Address.country)
+end
+
+products = Product.all.map { |p| p.id}
+
+Review.destroy_all
+
+250.times do |index|
+  Review.create!(author: Faker::Name.unique.name , rating: Faker::Number.between(from: 1, to: 5).to_i, content: Faker::Hipster.paragraph_by_chars(characters: 100) , product_id: products[rand(products.length)])
+
+end
+
+p "Created #{Product.count} products!"
+p "Created #{Review.count} reviews!"
