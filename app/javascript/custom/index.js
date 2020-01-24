ba9e35bc91f40acef58cb8aa6c67f5a866aff53c
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-24 15:54:49 -0800

    Attempt for bootstrap dropdown again.

--- app/javascript/custom/index.js
+++ app/javascript/custom/index.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-
-  function dropFunction() {
-  document.getElementsByClassName("dropdown-menu").classList.toggle("show");
-}
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-toggle')) {
-      let dropdowns = document.getElementsByClassName(".dropdown-item");
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-})
+// $(document).ready(function() {
+//
+//   function dropFunction() {
+//   document.getElementsByClassName("dropdown-menu").classList.toggle("show");
+// }
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.dropdown-toggle')) {
+//       let dropdowns = document.getElementsByClassName(".dropdown-item");
+//       let i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         let openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
+// })
