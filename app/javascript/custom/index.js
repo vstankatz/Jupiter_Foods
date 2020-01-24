04deb08c27f93173c644c636b306e9f68dc7d68c
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-24 15:24:55 -0800

    Attempt to add dropdown button.

--- app/javascript/custom/index.js
+++ app/javascript/custom/index.js
@@ -1,6 +1,18 @@
-import $ from 'jquery'
 $(document).ready(function() {
-  if (current_user.nil?) {
-    $(".private").show();
+
+  function dropFunction() {
+  document.getElementsByClassName("dropdown-menu").classList.toggle("show");
+}
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-toggle')) {
+      let dropdowns = document.getElementsByClassName(".dropdown-item");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
   }
 })
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,7 +5,9 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    jQuery: 'jquery/src/jquery',
+    'window.Tether': "tether",
+    Popper: ['popper.js', 'default']
   })
 )
 
