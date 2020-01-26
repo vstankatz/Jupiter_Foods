6d0199438c3efc3b0a2af2179bda363b33c15c37
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-25 17:55:18 -0800

    Complete scope testing.

--- spec/features/add_scope_spec.rb
+++ spec/features/add_scope_spec.rb
@@ -1,44 +1,29 @@
 require 'rails_helper'
 
 describe "scopes setup and teardown" do
-before :each do
-user = User.create(email: 'cool@cat.com', password: 'testing', admin: true)
-@product = Product.create({:name => "Apple Butter", :origin => "USA", :cost => 2.34, user_id: user.id})
-Review.create!(author: 'Bilbo Baggins' , rating: 4, content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.' , product_id: @product.id, user_id: user.id)
-end
+  before :each do
+    user = User.create(email: 'cool@cat.com', password: 'testing', admin: true)
+    @product = Product.create({:name => "Apple Butter", :origin => "USA", :cost => 2.34, user_id: user.id})
+    Review.create!(author: 'Bilbo Baggins' , rating: 4, content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.' , product_id: @product.id, user_id: user.id)
+  end
 
-describe "it views the products page through different scopes" do
-it 'should scope based off local' do
-  visit '/'
-  click_link 'Sign-In/Sign-Up'
-  click_link 'Sign up'
-  fill_in 'user[email]', :with => 'test@epicodus.com'
-  fill_in 'user[password]', :with => 'password'
-  fill_in 'user[password_confirmation]', :with => 'password'
-  check 'user[admin]'
-  click_on 'Sign up'
-  visit products_path
-  select('Made in the USA', from: 'scope')
-  expect(page).to have_content 'Apple Butter'
-end
+  describe "it views the products page through different scopes" do
+    it 'should scope based off local' do
+      visit products_path
+      select('Made in the USA', from: 'scope')
+      expect(page).to have_content 'Apple Butter'
+    end
 
-it 'should scope based off most reviews' do
-  visit '/'
-  click_link 'Sign-In/Sign-Up'
-  click_link 'Sign up'
-  fill_in 'user[email]', :with => 'test@epicodus.com'
-  fill_in 'user[password]', :with => 'password'
-  fill_in 'user[password_confirmation]', :with => 'password'
-  check 'user[admin]'
-  click_on 'Sign up'
-  visit products_path
-  select('Most Reviews', from: 'scope')
-  expect(page).to have_content 'Apple Butter'
-end
+    it 'should scope based off most reviews' do
+      visit products_path
+      select('Most Reviews', from: 'scope')
+      expect(page).to have_content 'Apple Butter'
+    end
 
-# it 'should scope based off most recently added' do
-#
-# end
-end
+    it 'should scope based off most recently added' do
+      visit products_path
+      select('Recently Added', from: 'scope')
+    end
+  end
 
 end
