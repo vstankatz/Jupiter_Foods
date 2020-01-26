511ab8af76cd04d54b98fac07ccb58bacaa4b209
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-25 19:51:51 -0800

    Update edit product spec for admin.

--- spec/features/edit_product_spec.rb
+++ spec/features/edit_product_spec.rb
@@ -5,24 +5,24 @@ describe "product editing" do
   before :each do
     @product = Product.create({:name => "Apple Butter", :origin => "USA", :cost => 2.34})
   end
-  #
-  # it "edits a product" do
-  #   visit '/'
-  #   click_link 'Sign-In/Sign-Up'
-  #   click_link 'Sign up'
-  #   fill_in 'user[email]', :with => 'test@epicodus.com'
-  #   fill_in 'user[password]', :with => 'password'
-  #   fill_in 'user[password_confirmation]', :with => 'password'
-  #   check 'user[admin]'
-  #   click_on 'Sign up'
-  #   visit product_path(@product)
-  #   click_on 'Edit product'
-  #   fill_in 'product[name]', :with => 'Peanut Butter'
-  #   fill_in 'product[origin]', :with => 'United State of America'
-  #   fill_in 'product[cost]', :with => '3'
-  #   click_on 'Update Product'
-  #   expect(page).to_not have_content "Peanut Butter"
-  # end
+
+  it "edits a product" do
+    visit '/'
+    click_link 'Sign-In/Sign-Up'
+    click_link 'Sign up'
+    fill_in 'user[email]', :with => 'test@epicodus.com'
+    fill_in 'user[password]', :with => 'password'
+    fill_in 'user[password_confirmation]', :with => 'password'
+    check 'user[admin]'
+    click_on 'Sign up'
+    visit product_path(@product)
+    click_on 'Edit product'
+    fill_in 'product[name]', :with => 'Peanut Butter'
+    fill_in 'product[origin]', :with => 'United State of America'
+    fill_in 'product[cost]', :with => '3'
+    click_on 'Update Product'
+    expect(page).to have_content 'Peanut Butter'
+  end
 
   it "returns error when user tries to edit a product" do
     visit '/'
