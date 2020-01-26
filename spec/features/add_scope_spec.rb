9de498f9c3fb94064b2ac9906fec9249b4f21b68
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-25 17:52:20 -0800

    Add second scope spec.

--- spec/features/add_scope_spec.rb
+++ spec/features/add_scope_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
+describe "scopes setup and teardown" do
+before :each do
+user = User.create(email: 'cool@cat.com', password: 'testing', admin: true)
+@product = Product.create({:name => "Apple Butter", :origin => "USA", :cost => 2.34, user_id: user.id})
+Review.create!(author: 'Bilbo Baggins' , rating: 4, content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.' , product_id: @product.id, user_id: user.id)
+end
+
 describe "it views the products page through different scopes" do
 it 'should scope based off local' do
   visit '/'
@@ -12,14 +19,26 @@ it 'should scope based off local' do
   click_on 'Sign up'
   visit products_path
   select('Made in the USA', from: 'scope')
-  expect(page).to have_content 'No products to display.'
+  expect(page).to have_content 'Apple Butter'
 end
 
-# it 'should scope based off most reviews' do
-#
-# end
-#
+it 'should scope based off most reviews' do
+  visit '/'
+  click_link 'Sign-In/Sign-Up'
+  click_link 'Sign up'
+  fill_in 'user[email]', :with => 'test@epicodus.com'
+  fill_in 'user[password]', :with => 'password'
+  fill_in 'user[password_confirmation]', :with => 'password'
+  check 'user[admin]'
+  click_on 'Sign up'
+  visit products_path
+  select('Most Reviews', from: 'scope')
+  expect(page).to have_content 'Apple Butter'
+end
+
 # it 'should scope based off most recently added' do
 #
 # end
+end
+
 end
