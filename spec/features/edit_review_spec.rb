89fbf8a3bd23823aae9471d6a0ff63a7ecbc3d22
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-25 21:38:01 -0800

    Adds error catching to edit review spec.

--- spec/features/edit_review_spec.rb
+++ spec/features/edit_review_spec.rb
@@ -57,7 +57,7 @@ end
     click_on 'Update Review'
     expect(page).to have_content "Author can't be blank"
   end
-
+  #
   # it "gives an error when no rating is entered" do
   #   visit '/'
   #   click_link 'Sign-In/Sign-Up'
@@ -73,41 +73,35 @@ end
   #   click_on 'Update Review'
   #   expect(page).to have_content "Rating can't be blank"
   # end
-  #
-  # it "gives an error when review is too short" do
-  #   visit '/'
-  #   click_link 'Sign-In/Sign-Up'
-  #   click_link 'Sign up'
-  #   fill_in 'user[email]', :with => 'test@epicodus.com'
-  #   fill_in 'user[password]', :with => 'password'
-  #   fill_in 'user[password_confirmation]', :with => 'password'
-  #   check 'user[admin]'
-  #   click_on 'Sign up'
-  #   visit product_path(@product)
-  #   click_link 'Create new review'
-  #   fill_in 'review[author]', :with => 'Samantha'
-  #   find(id: 'star_3').click
-  #   fill_in 'review[content]', :with => "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur."
-  #   click_on 'Create Review'
-  #   expect(page).to have_content 'Content is too long (maximum is 250 characters)'
-  # end
-  #
-  # it "gives an error when no review is too long" do
-  #   visit '/'
-  #   click_link 'Sign-In/Sign-Up'
-  #   click_link 'Sign up'
-  #   fill_in 'user[email]', :with => 'test@epicodus.com'
-  #   fill_in 'user[password]', :with => 'password'
-  #   fill_in 'user[password_confirmation]', :with => 'password'
-  #   check 'user[admin]'
-  #   click_on 'Sign up'
-  #   visit product_path(@product)
-  #   click_link 'Create new review'
-  #   fill_in 'review[author]', :with => 'Samantha'
-  #   find(id: 'star_3').click
-  #   fill_in 'review[content]', :with => "Lorem ipsum dolor sit amet, consectetur."
-  #   click_on 'Create Review'
-  #   expect(page).to have_content 'Content is too short (minimum is 50 characters)'
-  # end
+
+  it "gives an error when review is too long" do
+    visit '/'
+    click_link 'Sign-In/Sign-Up'
+    click_link 'Sign up'
+    fill_in 'user[email]', :with => 'test@epicodus.com'
+    fill_in 'user[password]', :with => 'password'
+    fill_in 'user[password_confirmation]', :with => 'password'
+    check 'user[admin]'
+    click_on 'Sign up'
+    visit edit_product_review_path(@product, @review)
+    fill_in 'review[content]', :with => "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur."
+    click_on 'Update Review'
+    expect(page).to have_content 'Content is too long (maximum is 250 characters)'
+  end
+
+  it "gives an error when no review is too short" do
+    visit '/'
+    click_link 'Sign-In/Sign-Up'
+    click_link 'Sign up'
+    fill_in 'user[email]', :with => 'test@epicodus.com'
+    fill_in 'user[password]', :with => 'password'
+    fill_in 'user[password_confirmation]', :with => 'password'
+    check 'user[admin]'
+    click_on 'Sign up'
+    visit edit_product_review_path(@product, @review)
+    fill_in 'review[content]', :with => "Lorem ipsum dolor sit amet, consectetur."
+    click_on 'Update Review'
+    expect(page).to have_content 'Content is too short (minimum is 50 characters)'
+  end
 
 end
