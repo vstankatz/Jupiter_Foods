c755e88dede03273dc1a895d4c5ed9ca7c11085c
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-26 01:38:22 -0800

    Update devise spec and view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :email, :password, presence: true
+  # validates :email, :password, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
