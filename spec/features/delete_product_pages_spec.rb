66052f9467ac23c73d0db9c44bc38e2a9183ad02
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-25 19:19:48 -0800

    Add spec for user product deletion.

--- spec/features/delete_product_pages_spec.rb
+++ spec/features/delete_product_pages_spec.rb
@@ -3,7 +3,7 @@ describe "product deletion" do
   before :each do
     @product = Product.create({:name => "Apple Butter", :origin => "USA", :cost => 2.34})
   end
-  it "gives deletes a product" do
+  it "deletes a product" do
     visit '/'
     click_link 'Sign-In/Sign-Up'
     click_link 'Sign up'
@@ -15,5 +15,17 @@ describe "product deletion" do
     visit product_path(@product)
     click_on 'Delete product'
     expect(page).to_not have_content "Apple Butter"
+  end
+
+  it "returns error when user tries to delete a product" do
+    visit '/'
+    click_link 'Sign-In/Sign-Up'
+    click_link 'Sign up'
+    fill_in 'user[email]', :with => 'test@epicodus.com'
+    fill_in 'user[password]', :with => 'password'
+    fill_in 'user[password_confirmation]', :with => 'password'
+    check 'user[admin]'
+    visit product_path(@product)
+    expect(page).to_not have_content "Delete product"
   end
 end
